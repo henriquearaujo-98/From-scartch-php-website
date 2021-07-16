2afaf9d477eb587d7dc24b7d9567998a943a53b0
Henrique Araujo <ha_98@msn.com>
2021-07-16 13:51:05 +0100

    Border radius on div that holds link back to main website.

--- includes/header.php
+++ includes/header.php
@@ -41,7 +41,9 @@
   </div>
   <div>
 </div>-->
-
+    <div class="back-to-personal">
+        <a href="https://henriquearaujo.pt/">Go back to henriquearaujo.pt</a>
+    </div>
   <header class="p-3 bg-dark text-white">
       <div class="container">
           <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
